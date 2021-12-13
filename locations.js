942007cedbe29415ddcce9e367499eff5da7cd32
Linda <thenegativejuan@gmail.com>
2021-12-13 14:45:04 -0800

    update

--- locations.js
+++ locations.js
@@ -1,21 +1,35 @@
 //Pull info from JSON file
-const data = require('./locations.json');
-console.log(data);
+//const data = require('./locations.json');
+//console.log(data);
 
-fetch("./locations.json")
-.then(response =>{
-  return response.json();
+//fetch("./locations.json")
+//.then(response =>{
+//  return response.json();
+//})
+//.then(data => console.log(data));
+
+fetch("locations.json").then(async response => {
+   try {
+      const data = await response.json()
+      console.log('response data?', data)
+   } catch (error) {
+      console.log('Error happened here!')
+      console.error(error)
+   }
 })
-.then(data => console.log(data));
 
 //Use onclick event to run the random location generator
 //when the user clicks on the button on main page
-document.getElementById("BUTTON") = function {randomLocation()};
+document.getElementById("foodButton").onclick = randomLocation();
 
 
 function randomLocation(){
+  //Redirect user using onclick event
+  //document.getElementbyId("foodButton").onclick = function (){
+  //  location.href = "/RESULTPAGE";
+  //};
   //Get location data from JSON file
-  var locations = JSON.parse(Merced);
+  var locations = JSON.parse("Merced");
   //Generate random number
   var randomNum = Math.floor((Math.random() * 40) + 1);
 
@@ -26,19 +40,8 @@ function randomLocation(){
   var placeRating = locations[randomNum.Rating];
 
   //Use HTML to print location info
-  document.getElementById("FILL").innerHTML = placeName;
-  document.getElementById("FILL").innerHTML = placeAddress;
-  document.getElementById("FILL").innerHTML = placeType;
-  document.getElementById("FILL").innerHTML = placeRating;
-
-  /*NOTE: Button will redirect to next page. Data must be loaded
-  before redirect occurs. To do this, we must add a timeout
-  and redirect the user using JS instead of HTMl*/
-
-  //Add Timeout of 5 seconds to load data
-
-  //Redirect user using onclick event
-  document.getElementbyId("BUTTON").onclick = function (){
-    location.href = "/RESULTPAGE";
-  };
+  document.getElementById("nameHTML").innerHTML = placeName;
+  document.getElementById("addressHTML").innerHTML = placeAddress;
+  document.getElementById("typeHTML").innerHTML = placeType;
+  document.getElementById("ratingHTML").innerHTML = placeRating;
 }
